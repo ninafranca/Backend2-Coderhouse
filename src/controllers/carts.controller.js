07c84ab4c2fa099f93743f65bdcb14577accfce7
Nina Spinelli <ninafspin@gmail.com>
2022-05-09 00:29:09 -0300

    carts controller, service y router listos en capas

--- src/controllers/carts.controller.js
+++ src/controllers/carts.controller.js
@@ -6,13 +6,67 @@ const getCartById = async (req, res) => {
         res.send(result);
     })
 }
-// const getCartById = async(req,res) =>{
-//     let id = req.params.cid;
-//     let cart = await cartService.getByWithPopulate({_id:id})
-//     console.log(cart);
-//     res.send({status:200,payload:cart})
-// }
+
+const getCartByUserId = async (req, res) => {
+    let userId = req.params.user_id;
+    await cartsService.getCartByUserId(userId).then(result => {
+        res.send(result);
+    })
+}
+
+const newCart = async (req, res) => {
+    let userId = req.params.user_id;
+    cartsService.newCart(userId).then(result => {
+        res.send(result);
+    })
+}
+
+const getCartByUserIdAddProd = async (req, res) => {
+    let userId = req.params.user_id;
+    let prodId = req.params.prod_id;
+    cartsService.getCartByUserIdAddProd(userId, prodId).then(result => {
+        res.send(result);
+    })
+}
+
+const saveProductById = async (req, res) => {
+    let prodId = req.params.id_prod;
+    let cartId = req.params.id_cart;
+    cartsService.saveProductById(prodId, cartId).then(result => {
+        res.send(result);
+    })
+}
+
+const deleteCartById = async (req, res) => {
+    let id = req.params.id;
+    cartsService.deleteCartById(id).then(result => {
+        res.send(result);
+    })
+}
+
+const deleteOneCartProd = async (req, res) => {
+    let prodId = req.params.id_prod;
+    let cartId = req.params.id_cart;
+    cartsService.deleteOneCartProd(cartId, prodId).then(result => {
+        res.send(result);
+    })
+}
+
+const deleteCartProd = async (req, res) => {
+    let prodId = req.params.id_prod;
+    let cartId = req.params.id_cart;
+    cartsService.deleteCartProd(cartId, prodId).then(result => {
+        res.send(result);
+    })
+}
 
 export default {
-    getCartById
+    getCartById,
+    getCartByUserId,
+    newCart,
+    getCartByUserIdAddProd,
+    saveProductById,
+    deleteCartById,
+    deleteOneCartProd,
+    deleteCartProd
 }
--- src/routes/carts.js
+++ src/routes/carts.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {carts} from "../daos/index.js";
+//import {carts} from "../daos/index.js";
 import cartsController from "../controllers/carts.controller.js";
 
 const router = express.Router();
@@ -14,65 +14,73 @@ router.get("/:cart_id/productos", cartsController.getCartById)
 //     })
 // })
 // Devuelve el carrito de un usuario
-router.get("/:user_id", (req, res) => {
-    let userId = req.params.user_id;
-    carts.getCartByUserId(userId).then(result => {
-        res.send(result);
-    })
-})
+router.get("/:user_id", cartsController.getCartByUserId)
+// router.get("/:user_id", (req, res) => {
+//     let userId = req.params.user_id;
+//     carts.getCartByUserId(userId).then(result => {
+//         res.send(result);
+//     })
+// })
 
 //POST 
 //Crea un carrito y devuelve su id
-router.post("/:user_id", (req, res) => {
-    let userId = req.params.user_id;
-    carts.newCart(userId).then(result => {
-        res.send(result);
-    }); 
-})
+router.post("/:user_id", cartsController.newCart)
+// router.post("/:user_id", (req, res) => {
+//     let userId = req.params.user_id;
+//     carts.newCart(userId).then(result => {
+//         res.send(result);
+//     }); 
+// })
 
 //Agrega producto a carrito de usuario
-router.post("/usuario/:user_id/producto/:prod_id", (req, res) => {
-    let userId = req.params.user_id;
-    let prodId = req.params.prod_id;
-    carts.getCartByUserIdAddProd(userId, prodId).then(result => {
-        res.send(result);
-    })
-})
+// REVISAR //
+router.post("/usuario/:user_id/producto/:prod_id", cartsController.getCartByUserIdAddProd)
+// router.post("/usuario/:user_id/producto/:prod_id", (req, res) => {
+//     let userId = req.params.user_id;
+//     let prodId = req.params.prod_id;
+//     carts.getCartByUserIdAddProd(userId, prodId).then(result => {
+//         res.send(result);
+//     })
+// })
 
 //incorpora productos al carrito de usuario por su id de producto
-router.post("/:id_cart/productos/:id_prod", (req, res) => {
-    let prodId = req.params.id_prod;
-    let cartId = req.params.id_cart;
-    carts.saveProdById(prodId, cartId).then(result => {
-        res.send(result);
-    })
-})
+router.post("/:id_cart/productos/:id_prod", cartsController.saveProductById)
+// router.post("/:id_cart/productos/:id_prod", (req, res) => {
+//     let prodId = req.params.id_prod;
+//     let cartId = req.params.id_cart;
+//     carts.saveProdById(prodId, cartId).then(result => {
+//         res.send(result);
+//     })
+// })
 
 //DELETE
 //Elimina un carrito
-router.delete("/:id", (req, res) => {
-    let id = req.params.id;
-    carts.deleteCartById(id).then(result => {
-        res.send(result);
-    })
-})
+router.delete("/:id", cartsController.deleteCartById)
+// router.delete("/:id", (req, res) => {
+//     let id = req.params.id;
+//     carts.deleteCartById(id).then(result => {
+//         res.send(result);
+//     })
+// })
 
 //Elimina una sola equivalencia de producto del carrito por su id de producto y de carrito
-router.delete("/:id_cart/producto/:id_prod", (req, res) => {
-    let prodId = req.params.id_prod;
-    let cartId = req.params.id_cart;
-    carts.deleteOneCartProd(cartId, prodId).then(result => {
-        res.send(result);
-    })
-})
+router.delete("/:id_cart/producto/:id_prod", cartsController.deleteOneCartProd)
+// router.delete("/:id_cart/producto/:id_prod", (req, res) => {
+//     let prodId = req.params.id_prod;
+//     let cartId = req.params.id_cart;
+//     carts.deleteOneCartProd(cartId, prodId).then(result => {
+//         res.send(result);
+//     })
+// })
 
 //Elimina todos los IDs de mismo producto del carrito por su id de producto y de carrito
-router.delete("/:id_cart/productos/:id_prod", (req, res) => {
-    let prodId = req.params.id_prod;
-    let cartId = req.params.id_cart;
-    carts.deleteCartProd(cartId, prodId).then(result => {
-        res.send(result);
-    })
-})
+router.delete("/:id_cart/productos/:id_prod", cartsController.deleteCartProd)
+// router.delete("/:id_cart/productos/:id_prod", (req, res) => {
+//     let prodId = req.params.id_prod;
+//     let cartId = req.params.id_cart;
+//     carts.deleteCartProd(cartId, prodId).then(result => {
+//         res.send(result);
+//     })
+// })
 
 export default router;
